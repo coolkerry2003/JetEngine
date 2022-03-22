e415dc8cbc71b47c87cf1cae330237da000e817b
User <User@PILOTGAEA-CHENG>
2022-03-22 17:40:17 +0800

    加入專案檔案。

---
+++ JetEngine/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace JetEngine
+{
+    class Program
+    {
+        static PerformanceCounter cpu = new PerformanceCounter(
+            "Processor", "% Processor Time", "_Total");
+        static PerformanceCounter memory = new PerformanceCounter(
+            "Memory", "% Committed Bytes in Use");
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("CPU: {0:n2}%,Memory: {1:n2}%", cpu.NextValue(), memory.NextValue());
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
